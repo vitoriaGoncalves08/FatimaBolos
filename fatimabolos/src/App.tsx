93941c57880f3277f0c4cc0178a8c7463be78751
Vitoria Goncalves Passos <VitoriaGoncalvesPassos@hotmail.com>
2025-04-21 15:40:32 -0300

    fix: ajustes para subir vercel

--- fatimabolos/src/App.tsx
+++ fatimabolos/src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
 import { Header } from './components/organisms/Header/header';
-import { TitleHome  } from './components/atoms/TitleHome/titlehome';
 import { Home } from './components/templates/home/home';
 
 function App() {
